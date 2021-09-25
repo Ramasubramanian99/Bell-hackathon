966020b7c7428e15184ba4dcec4737d674aea3f7
ramasubramanian99 <d.ramasubramanian.1997@gmail.com>
2021-09-25 17:16:28 -0400

    fixed a bug

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ import numpy as np
 app =Flask(__name__)
 
 loaded_model =  pickle.load(open('./finalized_model.sav', 'rb'))
-cols = ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall', 'soil_type']
+cols = ['temperature', 'humidity', 'ph', 'rainfall', 'soil_type']
 
 @app.route('/')
 def home():
-    return 'home.html'
+    return 'home'
 
 @app.route('/predict', methods=['POST'])
 def predict():
     int_features = [[x for x in request.form.values()]]
-    int_features =[[90, 42, 43, 20.87974371, 82.00274423, 6.502985292, 202.9355362, 0]]
     data_unseen = np.array(int_features)
     predicted_value = loaded_model.predict(data_unseen)
-    return str(predicted_value[0])
+    return jsonify(predicted_value[0])
 
 
 if __name__ == '__main__':
